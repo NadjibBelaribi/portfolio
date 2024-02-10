b71f812ad57052146d916174b645da4c21b7d3d4
Nadjib Belaribi <nbelaribi@etu.unistra.fr>
2024-02-10 20:56:52 +0100

    Updates

---
+++ precache-manifest.adf54f56900e726f8810f374226fa42f.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706465971fc44a41f85135f37ce4a69c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5353c465710acfb0cce0",
+    "url": "/portfolio/static/css/main.85f3c672.chunk.css"
+  },
+  {
+    "revision": "d401023182d380eb873c",
+    "url": "/portfolio/static/js/2.dbb22879.chunk.js"
+  },
+  {
+    "revision": "5353c465710acfb0cce0",
+    "url": "/portfolio/static/js/main.4c61fb2d.chunk.js"
+  },
+  {
+    "revision": "e1b765bfb7c37ab991a4",
+    "url": "/portfolio/static/js/runtime-main.7bdd33ea.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "24d06182139ba57cc88bca9fbf132076",
+    "url": "/portfolio/static/media/aircampus.24d06182.png"
+  },
+  {
+    "revision": "06deb5182cc5be43b4f38be12eab8fdb",
+    "url": "/portfolio/static/media/artex.06deb518.png"
+  },
+  {
+    "revision": "377d6016e6975fe7c4d2bc00860d35aa",
+    "url": "/portfolio/static/media/contact.377d6016.png"
+  },
+  {
+    "revision": "6d02b21c35e6f778c0cb6cfaa1701809",
+    "url": "/portfolio/static/media/education.6d02b21c.png"
+  },
+  {
+    "revision": "752a6099a2f369c4a1e9853a8a77d6e3",
+    "url": "/portfolio/static/media/esi.752a6099.png"
+  },
+  {
+    "revision": "fd836394d5276fcf080696b39f04e16b",
+    "url": "/portfolio/static/media/experience.fd836394.png"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/portfolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/portfolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/portfolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "66529b5a8fb17c2e9db2137f87deec39",
+    "url": "/portfolio/static/media/gdg.66529b5a.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/portfolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "be04a01479b2fbc12d6d937d9b9f6481",
+    "url": "/portfolio/static/media/home.be04a014.png"
+  },
+  {
+    "revision": "fbd72b83c5d874b4142ceacb9711bdb1",
+    "url": "/portfolio/static/media/logo.fbd72b83.png"
+  },
+  {
+    "revision": "e499d97b2d2e8b580642df21826aa6d3",
+    "url": "/portfolio/static/media/unistra.e499d97b.png"
+  }
+]);
