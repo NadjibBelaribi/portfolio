da08c104d62c05932402075826f73930163bf86d
Nadjib Belaribi <nbelaribi@etu.unistra.fr>
2024-02-11 00:22:53 +0100

    Update dataé

--- src/data.js
+++ src/data.js
@@ -13,14 +13,14 @@ const socialMediaLinks = [
   {
     name: "Github",
     link: "https://github.com/NadjibBelaribi",
-    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+    fontAwesomeIcon: "fa-github",
+    backgroundColor: "#181717",
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/nadjib-belaribi-ba898b13a/",
-    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+    fontAwesomeIcon: "fa-linkedin-in",
+    backgroundColor: "#0077B5",
   },
 ];
 
@@ -29,9 +29,9 @@ const skills = {
     {
       title: "Front End Development",
       skills: [
-        "♟ Building responsive website front end using React-Redux",
-        "♟ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
-        "♟ Creating application backend in Node, Express & Flask",
+        "♟ Mastery of HTML, CSS, React tools and extensions",
+        "♟ Build responsive and fast websites",
+        "♟ Testing applications with tools like JEST",
       ],
       softwareSkills: [
         {
@@ -88,9 +88,9 @@ const skills = {
     {
       title: "Back End Development",
       skills: [
-        "♟ Developing highly scalable production ready models for various deeplearning and statistical use cases",
-        "♟ Experience of working with Computer Vision and NLP projects",
-        "♟ Complex quantitative modelling for dynamic forecasting and time series analysis",
+        "♟ Design robust APIs and optimize solutions for performance and scalability.",
+        "♟ Develop automated tests to ensure business needs are met.",
+        "♟ Building reusable code and libraries for future use",
       ],
       softwareSkills: [
         {
@@ -156,10 +156,8 @@ const skills = {
     {
       title: "Cloud Infra & Architecture",
       skills: [
-        "♟ Experience working on multiple cloud platforms",
-        "♟ Hosting and maintaining websites on virtual machine instances along with integration of databases",
-        "♟ Deploying deep learning models on cloud to use on mobile devices",
-        "♟ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
+        "♟ Working on cloud platforms such as AWS",
+        "♟ CI/CD on Github using Docker and Kuberenetes",
       ],
       softwareSkills: [
         {
@@ -225,9 +223,9 @@ const degrees = {
       alt_name: "University Of Strasbourg",
       duration: "2018 - 2022",
       descriptions: [
-        "♟ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "♟ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "♟ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+        "♟ I started with a general degree in computer science where I learned the theoretical and practical foundations of computer science in terms of the design, creation and management of software in complex computer environments.",
+        "♟ I continued with a master's degree in software science and engineering in a work-study program. I deepened my knowledge of development through theoretical courses as well as individual and group educational projects, from the study of needs to the provision of validated code under constraints (security, tests and performance). ",
+        "♟ I had 2 experiences during this training where I applied my knowledge and learned how to test, deploy and document software.",
       ],
       website_link:
         "https://mathinfo.unistra.fr/formations/master/master-informatique/science-et-ingenierie-du-logiciel-sil",
@@ -239,9 +237,8 @@ const degrees = {
       alt_name: "Higher School Of Computer Science",
       duration: "2016 - 2018",
       descriptions: [
-        "♟ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "♟ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "♟ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "♟ I did the preparatory classes where I acquired a lot of mathematical and algorithmic knowledge (Analysis, Algebra, Algorithms and Data structures), it helps to optimize or resolve complex problems.",
+        "♟ I was part of the school's scientific groups whose goal is to share knowledge and work together to improve student life.",
       ],
       website_link: "https://www.esi.dz/",
     },
@@ -288,7 +285,7 @@ const experience = {
           logo_path: "gdg.png",
           duration: "October 2017 - Mai 2017",
           description:
-            "FreeCopy is the Start up from Indian Institute of Management, Ahmedabad. I have changed the integration of the whole app from Google to Firebase. I learnt the efﬁcient ways of Data communications like Retroﬁt, Eventbus etc. I experienced the real time start up. I learnt the Design thinking of UI on perspective of People.",
+            "I worked on the website interface and tried to make it easier to interact with the user. I also acquired skills in working in groups and developed a passion for producing high-quality software. I also worked on solving algorithms problems on websites like hackerRank and codeChef to improve my problem analysis and coding.",
           color: "#fc1f20",
         },
       ],
