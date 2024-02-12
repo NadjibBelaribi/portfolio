6f41d3d6bf5a2cc5ec549c7592f913ae97384c6d
Nadjib Belaribi <nbelaribi@etu.unistra.fr>
2024-02-12 10:34:20 +0100

    Correct CV

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const welcome = {
   description:
     " A 25 year old boy, passionate about software and latest technologies. I enjoy building sustainable and scalable systems, optimal and responsive websites.",
   resumeLink:
-    "https://drive.google.com/file/d/1n5gIycYUmkaQ76MEdsE11MU3wcs2PICg/view?usp=sharing",
+    "https://drive.google.com/file/d/1Q69l8bPucW1lNO4VqLB-w6U2KL4TJxe0/view?usp=sharing",
   githubProfile: "https://github.com/NadjibBelaribi",
 };
 
